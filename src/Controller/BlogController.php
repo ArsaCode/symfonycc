49672010eb21a18f5454ff954ee89a6a3f78a08f
Arsalan Ghassemi <contact@arsadevs.com>
2020-10-15 02:25:40 +0200

    Added dynamic routes for single article

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,34 +4,30 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog", name="blog")
-     */
-    public function index()
-    {
-        return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
-        ]);
-    }
-    
-    /**
      * @Route("/", name="home")
      */
-    public function home()
+    public function index(ArticleRepository $repo)
     {
+        $articles = $repo->findAll();
         return $this->render('blog/home.html.twig', [
             "title" => "Bienvenue dans ce blog",
+            "articles" => $articles,
         ]);
     }
 
     /**
-     * @Route("/blog/12", name="show")
+     * @Route("/blog/{id}", name="show")
      */
-    public function show()
+    public function show(Article $article)
     {
-        return $this->render('blog/article.html.twig');
+        return $this->render('blog/article.html.twig', [
+            "article" => $article,
+        ]);
     }
 }
