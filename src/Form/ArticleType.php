e8efc570549e865d95ab4c794ec8aace07635c99
Arsalan Ghassemi <contact@arsadevs.com>
2020-11-30 21:53:08 +0100

    Added User entity and registration form

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,6 +14,10 @@ class ArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'title'
+            ])
             ->add('title')
             ->add('content')
             ->add('image')
