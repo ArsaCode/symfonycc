d87144d7393d81b8abcb78182f1d1ac962ecc3e0
Arsalan Ghassemi <contact@arsadevs.com>
2020-11-30 01:27:00 +0100

    Added modifications of articles

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,6 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request;
 
 class BlogController extends AbstractController
 {
@@ -23,17 +27,30 @@ class BlogController extends AbstractController
 
     /**
      * @Route("/blog/new", name="create")
+     * @Route("/blog/{id}/edit", name="edit")
      */
-    public function create()
+    public function form(Article $article = null, Request $request, EntityManagerInterface $manager)
     {
-        $article = new Article();
         $form = $this->createFormBuilder($article)
                     ->add('title')
                     ->add('content')
                     ->add('image')
                     ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            if(!$article->getId()) {
+                $article->setDate(new \DateTime());
+            }
+            $manager->persist($article);
+            $manager->flush();
+            return $this->redirectToRoute('show', ['id' => $article->getId()]);
+        }
+
         return $this->render('blog/create.html.twig', [
             "formArticle" => $form->createView(),
+            "editing" => $article ? true : false
         ]);
     }
 
